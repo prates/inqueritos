3f7c0508c565e5ea82ddb2dee3899e5e0dcc630b
Iara Prates <iaramirandaprates@gmail.com>
2021-07-24 19:31:32 -0300

    add query in bd

--- view.py
+++ view.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+from typing import Tuple
+import json
 
 from flask import request, Flask, render_template
 from flask_wtf import FlaskForm
@@ -12,7 +14,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder="templates")
 
 app.config['SECRET_KEY'] = 'you-will-never-guess'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysecret@127.0.0.1:3306/inqueritos'
 db = SQLAlchemy(app)
 
 
@@ -27,20 +29,27 @@ class Inquerito(db.Model):
 
 class ControllerInquerito():
 
-    def salva_inquerito(vitima, bandido, data_abertura_str):
+    def salva_inquerito(self, vitima, bandido, data_abertura_str):
         data_abertura = datetime.strptime(data_abertura_str, "%d/%m/%Y")
         data_vencimento = data_abertura + timedelta(days=30)
         inquerito = Inquerito(bandido=bandido, vitima=vitima, 
                             data_abertura=data_abertura,
                             data_vencimento=data_vencimento)
         db.session.add(inquerito)
-        db.commit()
+        db.session.commit()
 
-    def lista_inqueritos():
+    def lista_inqueritos(self):
         inqueritos = Inquerito.query.all()
-        return inqueritos            
+        inqueritos_render = []
+        for inquerito in inqueritos:
+            inqueritos_render.append({"id": inquerito.id,    
+                                        "bandido": inquerito.bandido,
+                                        "vitima": inquerito.vitima,
+                                        "data_abertura": inquerito.data_abertura.strftime("%Y-%m-%d"),
+                                        "data_vencimento": inquerito.data_vencimento.strftime("%Y-%m-%d")})
+        return inqueritos_render            
 
-    def prorroga_inquerito(inquerito_id):
+    def prorroga_inquerito(self, inquerito_id):
         inquerito = Inquerito.query.filter_by(id=inquerito_id).first()
         
 
@@ -53,6 +62,12 @@ class FormInquerito(FlaskForm):
     submit = SubmitField("Criar Inquerito")
 
 
+@app.route("/consulta", methods=["GET"])
+def consulta_iquerito():
+    controller = ControllerInquerito()
+    lista = controller.lista_inqueritos()
+    return json.dumps(lista)
+
 @app.route('/', methods=['POST', 'GET'])
 def novo_inquerito():
     form = FormInquerito()
@@ -61,7 +76,8 @@ def novo_inquerito():
         bandido = request.form["bandido"]
         data_inquerito = request.form["data_inquerito"]
         controller = ControllerInquerito()
-
+        controller.salva_inquerito(vitima, bandido, data_inquerito)
+        return "Inquerito adicionado"
     return render_template("cria_inquerito.html", form=form)
 
 
